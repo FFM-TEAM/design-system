bd1db840f587dfe85efddcaae6e07df957680bdd
s-ong-c <somony9292@gmail.com>
2021-02-10 21:15:26 +0900

    feat: Icon storybook

---
+++ src/stories/Icon.stories.tsx
@@ -0,0 +1,13 @@
+import { Meta, Story } from '@storybook/react/types-6-0'
+import React from 'react'
+
+import Icon from '../components/Icon'
+
+export default {
+  title: 'Components/Icon',
+  component: Icon,
+} as Meta
+
+const Template: Story = () => <Icon name="star" />
+
+export const LoggedIn = Template.bind({})
